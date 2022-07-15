52a1b53c5b08059587770a28c6c7444ec0565e41
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-07-15 07:49:02 -0600

    initial commit.

---
+++ DataListItemIdentification.kt
@@ -0,0 +1,11 @@
+/**
+ * *Copyright (C), 2022-2023, Sara Echeverria (bl33h)
+    * @author Sara Echeverria
+    * FileName: DataListItemIdentification
+    @version: I
+    - Creacion: 15/07/2022
+    - Ultima modificacion: 20/05/2022
+ */
+fun main() {
+    println("Hello, world!!!")
+}
