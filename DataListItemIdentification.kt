5126ba15cfd85be74ecc34490a68c598f41d490b
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-07-16 21:09:01 -0600

    when's update + comments.

--- DataListItemIdentification.kt
+++ DataListItemIdentification.kt
@@ -4,7 +4,7 @@
     * FileName: DataListItemIdentification
     @version: I
     - Creacion: 15/07/2022
-    - Ultima modificacion: 20/05/2022
+    - Ultima modificacion: 16/07/2022
  */
 
 // Data class ---
@@ -16,25 +16,37 @@ data class ItemData(
 )
 // -----------------------
 
+// Metodo main
 fun main() {
     val result = processList(listOf(25, "Hola", null, false))
     println(result)
 }
-
+// Metodo para procesar la lista y retornar el resultado
 fun processList(inputList: List<Any?>?): List<ItemData>? {
 	// Variable lista de los items de la otra cadena
-    ArrayList<ItemData>()
+    resultList = ArrayList<ItemData>()
     // Contador iniciado en 0
     var i: Int = 0
     if (inputList == null) return null
     for(originalValue in inputList){
         when(originalValue){
-            is String -> println("String")
-            is Int -> println("entero")
-            is Boolean -> println("Type: booleano")
+            // Variables receptivas
+            var types: String? = " "
+            var infor = String? = " "
+            // Ajustes del contador
+            i = i + 1
+            is String -> //println("String")
+            { }
+            is Int ->
+            { } //println("entero")
+            is Boolean -> 
+            { }//println("Type: booleano")
             else -> println("null")
         }
     }
+    // -----------------------
     
+    // Retorno de la lista con los valores implementados
     return emptyList()
 }
+// -----------------------
